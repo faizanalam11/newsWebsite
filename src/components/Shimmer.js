7541c47de2c75168179ec613742406fa46ff6e1a
Faizan Alam <faizanalam416@gmail.com>
2024-05-07 13:50:02 +0530

    updating shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,7 +1,7 @@
 const Shimmer = () => {
     return (
-        <div className="flex flex-wrap mx-16 m-auto">
-            {Array(20).fill("").map((e, index) => <div key={index} className="bg-gray-300 w-52 h-52 m-2"></div>)}
+        <div className="flex flex-wrap w-5/6 justify-center items-center m-auto">
+            {Array(20).fill("").map((e, index) => <div key={index} className="bg-gray-300 w-72 m-3 p-2 h-72"></div>)}
         </div>
     );
 }
