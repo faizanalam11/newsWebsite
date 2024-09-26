30f7a39ae8e1eb2ba68553380f61848a319e8102
Faizan Alam <faizanalam416@gmail.com>
2024-09-26 12:24:38 +0530

    API changes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import { useState, useEffect } from "react";
 import { url } from "./utils/constants";
-import { apiKey } from "./utils/constants";
 import Footer from "./components/Footer";
 
 const AppLayout = () => {
@@ -17,7 +16,7 @@ const AppLayout = () => {
 
     async function getNewsData(query) {
         try{
-            const newsData = await fetch(`${url}${query}&apiKey=${apiKey}`);
+            const newsData = await fetch(`${url}${query}&apiKey=${process.env.API_KEY}`);
             const jsonNews = await newsData.json();
             setFilteredData(jsonNews?.articles);
         }
